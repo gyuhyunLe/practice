22cfa8209cb77c11ca8350a85c8c920ed49b2f6c
gyuhyunLe <dlrbgus95@gmail.com>
2023-07-26 13:06:30 +0900

    paging 수정

--- src/main/java/com/pokemonreview/api/models/PokemonType.java
+++ src/main/java/com/pokemonreview/api/models/PokemonType.java
@@ -1,8 +1,13 @@
 package com.pokemonreview.api.models;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum PokemonType {
     NORMAL, FIRE, WATER, ELECTRIC, GRASS,
     ICE, FIGHTING, POISON, GROUND, FLYING,
     PSYCHIC, BUG, ROCK, GHOST, DRAGON,
     DARK, STEEL, FAIRY
+
+
 }
